da1b532ab58c5282551552acb06047e0cd4ab560
BORHOMJKJ0 <hborhom89@gmail.com>
2025-01-01 02:21:08 +0300

    Allow guest to show and order stores

--- app/Services/StoreService.php
+++ app/Services/StoreService.php
@@ -36,9 +36,6 @@ class StoreService
     public function getAllStores(Request $request)
     {
         try {
-            if (! $this->checkSuperAdmin()) {
-                $this->checkGuest();
-            }
             $items = $request->query('items', 20);
             $stores = $this->storeRepository->getAll($items);
 
@@ -127,9 +124,6 @@ class StoreService
 
     public function getStoreById(Store $store): JsonResponse
     {
-        if (! $this->checkSuperAdmin()) {
-            $this->checkGuest();
-        }
         if ($store->status === 'pending') {
             if ($this->checkSuperAdmin()) {
                 return ResponseHelper::jsonResponse([], 'Mr.SuperAdmin : We are waiting your response for creating this store .', 404, false);
@@ -224,9 +218,6 @@ class StoreService
     public function getStoresOrderedBy($column, $direction, Request $request)
     {
         try {
-            if (! $this->checkSuperAdmin()) {
-                $this->checkGuest();
-            }
             $lang = $request->header('lang', 'en');
             $nameColumn = $lang === 'ar' ? 'name_ar' : 'name_en';
             $validColumns = [$nameColumn, 'location', 'created_at', 'updated_at'];
